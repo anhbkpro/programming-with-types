5ef84171a7826d4fc9a843635df8f24012a23bc5
AnhLai <laingocanh1990@gmail.com>
2021-05-08 11:46:50 +0700

    compiler will throw error if we try to access the private

---
+++ Chapter1/08_encapsulation.ts
@@ -0,0 +1,23 @@
+namespace Encapsulation {
+    class SafeDivisor {
+        private divisor: number = 1;
+
+        setDivisor(value: number) {
+            if (value == 0) throw new Error("x should not be 0");
+
+            this.divisor = value;
+        }
+
+        divide(x: number): number {
+            return  x / this.divisor;
+        }
+    }
+
+    function exploit(): number{
+        let sd = new SafeDivisor();
+        //sd.divisor = 0; // we can not access private now
+        return sd.divide(42);
+    }
+
+    console.log(exploit())
+}
